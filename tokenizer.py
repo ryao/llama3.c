aa0847f8574c3859d2d8e200ccbdeecaa5067bde
James Delancey <jamesdelanceyjr@gmail.com>
2024-04-29 00:47:25 -0700

    Update tokenizer.py for LLaMA 3 base and instruct

--- tokenizer.py
+++ tokenizer.py
@@ -1,42 +1,84 @@
 # Taken from llama code and lightly modified
 # Copyright (c) Meta Platforms, Inc. and affiliates.
-# This software may be used and distributed according to the terms of the Llama 2 Community License Agreement.
+# This software may be used and distributed according to the terms of the Llama 3 Community License Agreement.
 
+import array
 import os
 import struct
 import argparse
+from pathlib import Path
 from typing import List
 
-from sentencepiece import SentencePieceProcessor
+import tiktoken
+from tiktoken.load import load_tiktoken_bpe
 
-TOKENIZER_MODEL = "tokenizer.model" # the llama sentencepiece tokenizer model
+TOKENIZER_MODEL = "tokenizer.model"  # the llama tiktoken tokenizer model
+
 
 class Tokenizer:
+    pat_str = r"(?i:'s|'t|'re|'ve|'m|'ll|'d)|[^\r\n\p{L}\p{N}]?\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]+[\r\n]*|\s*[\r\n]+|\s+(?!\S)|\s+"
+
     def __init__(self, tokenizer_model=None):
         model_path = tokenizer_model if tokenizer_model else TOKENIZER_MODEL
         assert os.path.isfile(model_path), model_path
-        self.sp_model = SentencePieceProcessor(model_file=model_path)
+        mergeable_ranks = load_tiktoken_bpe(model_path)
         self.model_path = model_path
 
         # BOS / EOS token IDs
-        self.n_words: int = self.sp_model.vocab_size()
-        self.bos_id: int = self.sp_model.bos_id()
-        self.eos_id: int = self.sp_model.eos_id()
-        self.pad_id: int = self.sp_model.pad_id()
-        #print(f"#words: {self.n_words} - BOS ID: {self.bos_id} - EOS ID: {self.eos_id}")
-        assert self.sp_model.vocab_size() == self.sp_model.get_piece_size()
+        num_base_tokens = len(mergeable_ranks)
+        num_reserved_special_tokens = 256
 
-    def encode(self, s: str, bos: bool, eos: bool) -> List[int]:
+        special_tokens = [
+            "<|begin_of_text|>",
+            "<|end_of_text|>",
+            "<|reserved_special_token_0|>",
+            "<|reserved_special_token_1|>",
+            "<|reserved_special_token_2|>",
+            "<|reserved_special_token_3|>",
+            "<|start_header_id|>",
+            "<|end_header_id|>",
+            "<|reserved_special_token_4|>",
+            "<|eot_id|>",  # end of turn
+        ] + [
+            f"<|reserved_special_token_{i}|>"
+            for i in range(5, num_reserved_special_tokens - 5)
+        ]
+        self.special_tokens = {
+            token: num_base_tokens + i for i, token in enumerate(special_tokens)
+        }
+        self.model = tiktoken.Encoding(
+            name=Path(model_path).name,
+            pat_str=self.pat_str,
+            mergeable_ranks=mergeable_ranks,
+            special_tokens=self.special_tokens,
+        )
+        self.n_words = self.model.n_vocab
+        self.bos_id = self.special_tokens["<|begin_of_text|>"]
+        self.eos_id = self.special_tokens["<|end_of_text|>"]
+        self.pad_id = -1
+        self.stop_tokens = {
+            self.special_tokens["<|end_of_text|>"],
+            self.special_tokens["<|eot_id|>"],
+        }
+
+    def encode(
+        self, s: str, bos: bool, eos: bool, allowed_special, disallowed_special
+    ) -> List[int]:
         assert type(s) is str
-        t = self.sp_model.encode(s)
+        self.model.encode(
+            substr,
+            allowed_special=allowed_special,
+            disallowed_special=disallowed_special,
+        )
+
         if bos:
-            t = [self.bos_id] + t
+            t.insert(0, self.bos_id)
         if eos:
-            t = t + [self.eos_id]
+            t.append(self.eos_id)
         return t
 
     def decode(self, t: List[int]) -> str:
-        return self.sp_model.decode(t)
+        return self.model.decode(t)
 
     def export(self):
 
@@ -45,16 +87,9 @@ class Tokenizer:
         for i in range(self.n_words):
 
             # decode the token and light postprocessing
-            t = self.sp_model.id_to_piece(i)
-            s = self.sp_model.get_score(i)
-            if i == self.bos_id:
-                t = '\n<s>\n'
-            elif i == self.eos_id:
-                t = '\n</s>\n'
-            t = t.replace('▁', ' ') # sentencepiece uses this character as whitespace
-            b = t.encode('utf-8') # bytes of this token, utf-8 encoded
-
-            tokens.append(b)
+            t = self.model.decode_single_token_bytes(i)
+            s = i
+            tokens.append(t)
             scores.append(s)
 
         # record the max token length
@@ -62,16 +97,19 @@ class Tokenizer:
 
         # write to a binary file
         # the tokenizer.bin file is the same as .model file, but .bin
-        tokenizer_bin = self.model_path.replace('.model', '.bin')
-        with open(tokenizer_bin, 'wb') as f:
+        tokenizer_bin = self.model_path.replace(".model", ".bin")
+        with open(tokenizer_bin, "wb") as f:
             f.write(struct.pack("I", max_token_length))
             for bytes, score in zip(tokens, scores):
                 f.write(struct.pack("fI", score, len(bytes)))
                 f.write(bytes)
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-t", "--tokenizer-model", type=str, help="optional path to custom tokenizer ")
+        "-t", "--tokenizer-model", type=str, help="optional path to custom tokenizer "
+    )
     args = parser.parse_args()
 
     t = Tokenizer(args.tokenizer_model)
