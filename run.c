9522fdac140cd935263d053d35b79a685b331feb
Richard Yao <richard.yao@alumni.stonybrook.edu>
2024-11-29 11:04:04 -0500

    Align memory allocations via new calloc_aligned() helper
    Signed-off-by: Richard Yao <richard.yao@alumni.stonybrook.edu>

--- run.c
+++ run.c
@@ -74,19 +74,32 @@ typedef struct {
   ssize_t file_size; // size of the checkpoint file in bytes
 } Transformer;
 
+void *calloc_aligned(size_t num, size_t size) {
+  size_t total_size = num * size;
+
+  void *ptr = NULL;
+  if (posix_memalign(&ptr, 64, total_size) != 0) {
+    return NULL;
+  }
+
+  memset(ptr, 0, total_size);
+
+  return ptr;
+}
+
 void malloc_run_state(RunState *s, Config *p) {
   // we calloc instead of malloc to keep valgrind happy
   int kv_dim = (p->dim * p->n_kv_heads) / p->n_heads;
-  s->x = calloc(p->dim, sizeof(float));
-  s->xb = calloc(p->dim, sizeof(float));
-  s->xb2 = calloc(p->dim, sizeof(float));
-  s->hb = calloc(p->hidden_dim, sizeof(float));
-  s->hb2 = calloc(p->hidden_dim, sizeof(float));
-  s->q = calloc(p->dim, sizeof(float));
-  s->key_cache = calloc(p->n_layers * p->seq_len * kv_dim, sizeof(float));
-  s->value_cache = calloc(p->n_layers * p->seq_len * kv_dim, sizeof(float));
-  s->att = calloc(p->n_heads * p->seq_len, sizeof(float));
-  s->logits = calloc(p->vocab_size, sizeof(float));
+  s->x = calloc_aligned(p->dim, sizeof(float));
+  s->xb = calloc_aligned(p->dim, sizeof(float));
+  s->xb2 = calloc_aligned(p->dim, sizeof(float));
+  s->hb = calloc_aligned(p->hidden_dim, sizeof(float));
+  s->hb2 = calloc_aligned(p->hidden_dim, sizeof(float));
+  s->q = calloc_aligned(p->dim, sizeof(float));
+  s->key_cache = calloc_aligned(p->n_layers * p->seq_len * kv_dim, sizeof(float));
+  s->value_cache = calloc_aligned(p->n_layers * p->seq_len * kv_dim, sizeof(float));
+  s->att = calloc_aligned(p->n_heads * p->seq_len, sizeof(float));
+  s->logits = calloc_aligned(p->vocab_size, sizeof(float));
   // ensure all mallocs went fine
   if (!s->x || !s->xb || !s->xb2 || !s->hb || !s->hb2 || !s->q || !s->key_cache || !s->value_cache || !s->att || !s->logits) {
     fprintf(stderr, "malloc failed!\n");
